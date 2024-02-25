567f490711cd0c26b59a84442a482e6c68d0d7ed
diptesh17 <deorediptesh1883@gmail.com>
2024-02-25 16:48:33 +0530

    Setup the basic route structure

--- src/index.js
+++ src/index.js
@@ -5,12 +5,23 @@ const app = express();
 
 const { PORT } = require("./config/server");
 
+const apiRoutes = require("./routes/index");
+
+const db = require("./models/index");
+
 const serverSetup = () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
+  app.use("/api", apiRoutes);
+
   app.listen(PORT, () => {
     console.log(`Server Started on Port : ${PORT}`);
+
+    if (process.env.DB_SYNC) {
+      db.sequelize.sync;
+      ({ alter: true });
+    }
   });
 };
 
